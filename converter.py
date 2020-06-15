1a6b5ef036c6d463ecdb161a4b591b077f8d179f
Pedro Marcelo <pedrocarvalhomarcelo@hotmail.com>
2020-06-15 20:35:30 -0300

    solving minor bugs

--- converter.py
+++ converter.py
@@ -1,19 +1,17 @@
 # types: float8 float32 double64
-
-
-
 def get_signal(dec_num):
     if dec_num < 0:
         return 1
     else:
         return 0
 
-def frac_to_bin(frac_num,type):
-    if type == "float8":
+
+def frac_to_bin(frac_num, numeric_type):
+    if numeric_type == "float8":
         maxsize = 4
-    elif type == "float32":
+    elif numeric_type == "float32":
         maxsize = 23
-    elif type == "double64":
+    elif numeric_type == "double64":
         maxsize = 52
     binfrac = ""
     n = frac_num
@@ -28,57 +26,60 @@ def frac_to_bin(frac_num,type):
             break
     return binfrac 
 
-def convert_int_to_bin(dec_num,type):
+
+def convert_int_to_bin(dec_num, numeric_type):
     str_num = str(dec_num)
     frac_num = "0"
     if "." in str_num:
         str_num = str_num.split(".")
         dec_num = float(str_num[0])
         frac_num = float("0."+str_num[1])
-        frac_num = frac_to_bin(frac_num,type)    
+        frac_num = frac_to_bin(frac_num, numeric_type)
     num_str = str(bin(int(dec_num)))
 
     if dec_num >= 0:
-        bin_num = num_str.replace("0b", "")
+        binary_num = num_str.replace("0b", "")
     else:
-        bin_num = num_str.replace("-0b", "")
-    return bin_num, frac_num
+        binary_num = num_str.replace("-0b", "")
+    return binary_num, frac_num
 
 
-def get_exp(bin_num):
-    return bin_num.count('0') + bin_num.count('1') - 1
+def get_exp(binary_num):
+    return binary_num.count('0') + binary_num.count('1') - 1
 
-def create_exp(bin_num, num_type):
-    scy_exp = get_exp(bin_num)
-    if num_type == "float8":
+
+def create_exp(binary_num, numeric_type):
+    scy_exp = get_exp(binary_num)
+    if numeric_type == "float8":
         exp = 7 + scy_exp
         return bin(exp).replace("0b", "")
-    elif num_type == "float32":
+    elif numeric_type == "float32":
         exp = 127 + scy_exp
         return bin(exp).replace("0b", "")
-    elif num_type == "double64":
+    elif numeric_type == "double64":
         exp = 1023 + scy_exp
         return bin(exp).replace("0b", "")
 
-def create_mantissa(bin_num, type):
+
+def create_mantissa(binary_num, numeric_type):
     float_man = str
-    if type == "float8":
-        bin_num = str(bin_num) + str("0" * 5)
-        float_man = bin_num[1:4]
+    if numeric_type == "float8":
+        binary_num = str(binary_num) + str("0" * 5)
+        float_man = binary_num[1:4]
         return float_man
-    elif type == "float32":
-        bin_num = str(bin_num) + str("0" * 24)
-        float_man = bin_num[1:24]
+    elif numeric_type == "float32":
+        binary_num = str(binary_num) + str("0" * 24)
+        float_man = binary_num[1:24]
         return float_man
-    elif type == "double64":
-        bin_num = str(bin_num) + str("0" * 53)
-        float_man = bin_num[1:53]
+    elif numeric_type == "double64":
+        binary_num = str(binary_num) + str("0" * 53)
+        float_man = binary_num[1:53]
         return float_man
-    
 
-def printNumero(signal,exp,mantissa):
-    print("signal: " + str(signal) + " | expoent: " + str(exp) + " | mantissa: " + str(mantissa) + "\n")
 
+def print_number(binary_signal, binary_exp, bynayr_mantissa):
+    print("signal: " + str(binary_signal) + " | expoent: " + str(binary_exp) + " | mantissa: " + str(bynayr_mantissa) +
+          "\n")
 
 
 resp = int
@@ -92,32 +93,26 @@ while resp != 0:
     type your numeric answer here: """))
     if resp == 1:
         print("you want to calculate a floating point (8bits)")
-        type = "float8"
+        num_type = "float8"
        
     if resp == 2:
         print("you want to calculate IEE 745 single precision (32 bits) number")
-        type = "float32" 
+        num_type = "float32"
 
     if resp == 3:
         print("you want to calculate a IEE 745 double precision (64bits) number")
         print("you want to calculate IEE 745 single precision (32 bits) number")
-        type = "double64"
+        num_type = "double64"
 
     if resp == 4:
         print("you want to calculate all numbers")
     if resp == 0:
-        print("bye !" + "\n" )
+        print("bye !" + "\n")
 
-
-
-    num = float(input("enter a  decimal number number: "))
-    bin_num, bin_frac = convert_int_to_bin(num,type)
+    num = float(input("enter a  decimal number: "))
+    bin_num, bin_frac = convert_int_to_bin(num, num_type)
     signal = get_signal(num)
-    exp = create_exp(bin_num, type)
-    mantissa = create_mantissa(bin_num+bin_frac, type)
-    printNumero(signal,exp,mantissa)
+    exp = create_exp(bin_num, num_type)
+    mantissa = create_mantissa(bin_num+bin_frac, num_type)
+    print_number(signal, exp, mantissa)
 
-
-#num = float(input("enter a number: "))
-#bin_num = convert_int_to_bin(num)
-#print(str(get_signal(num)) + " | " + str((create_exp(bin_num, "float8")) + " | " + str(create_mantissa(bin_num, "float8"))))
